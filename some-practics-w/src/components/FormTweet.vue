7e4223691791434a7707da50e7c661e19139de37
Stephan <stefanstark77@gmail.com>
2022-05-16 14:00:20 +0300

    rewrite to composition API

--- some-practics-w/src/components/FormTweet.vue
+++ some-practics-w/src/components/FormTweet.vue
@@ -6,22 +6,46 @@
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
-    data() {
-        return {
-            body: '',
-        }
-    }, 
-    methods: {
-        onSubmit() {
-            this.$emit('onMessage',{
-                text: this.body,
+    emits: ['onSubmit'],
+    setup(_, {emit}) {
+        const body = ref('');
+
+        function onSubmit() {
+            emit('onMessage',{
+                text: body.value,
                 date: new Date(Date.now()).toLocaleString(),
                 author: 'Stepan',
                 likes: 0,
             });
-            this.body = '';
+            body.value = '';
+        }
+
+        return {
+            body,
+            onSubmit,  
         }
     }
 }
+
+// export default {
+//     data() {
+//         return {
+//             body: '',
+//         }
+//     }, 
+//     methods: {
+//         onSubmit() {
+//             this.$emit('onMessage',{
+//                 text: this.body,
+//                 date: new Date(Date.now()).toLocaleString(),
+//                 author: 'Stepan',
+//                 likes: 0,
+//             });
+//             this.body = '';
+//             //isVisible = !isVisible;
+//         }
+//     }
+//}
 </script>
