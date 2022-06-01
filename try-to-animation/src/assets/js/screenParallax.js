cfdd78cb66ee07acbfd1c99db8895f131ad19f25
Stephan <stefanstark77@gmail.com>
2022-06-01 23:07:42 +0300

    +animation for h1 h2 text

--- try-to-animation/src/assets/js/screenParallax.js
+++ try-to-animation/src/assets/js/screenParallax.js
@@ -3,11 +3,9 @@
 import $ from 'jquery';
 
 function screenParallax($mainPage) {
-	const speedScroll = 10;
-
+	const speedScroll = 11.5;
 	let $scrollPosition = $(window).scrollTop();
-	console.log($mainPage.height(), $scrollPosition);		
-	if($mainPage.height() > $scrollPosition) {
+	if($mainPage.height() >= $scrollPosition) {
 		$($mainPage).css(
 			"transform", `translateY(${-$scrollPosition*.25 / speedScroll }vh)`
 		)
--- try-to-animation/src/pages/MainPage.vue
+++ try-to-animation/src/pages/MainPage.vue
@@ -1,24 +1,34 @@
 <template>
 	<HeaderNavigation></HeaderNavigation>
-	<section class="mainPage h-full flex flex-col items-center sticky top-0 -z-10">
+	<section class="mainPage min-h-full flex flex-col items-center sticky top-0 -z-10">
 		<!-- <div class="mainPage__content "> -->
-			<div class="mainPage__text flex-auto text-center flex flex-col justify-center md:gap-3 gap-2">
-				<h1 class="mainPage__title text-6xl font-semibold">
-					Keqing
-				</h1>
-				<h2 class="mainPage__subTitle text-4xl">
-					Driving Thunder   
-				</h2>
-			</div>		
-			<div class="mainPage__icon mb-5">
-				<MainPageArrowDown></MainPageArrowDown>
-			</div>
+			<div class="mainPage__text flex-auto text-center flex flex-col justify-center md:gap-3 gap-2 ">
+				<div class="overflow-hidden p-1">
+					<Transition appear name="slide-text">
+						<h1 class="mainPage__title text-6xl font-semibold ">
+							<span>Keqing</span>
+						</h1>	
+					</Transition>
+				</div>
+				<div class="overflow-hidden">
+					<Transition appear name="slide-text">
+						<h2 class="mainPage__subTitle text-4xl">
+							Driving Thunder   
+						</h2>
+					</transition>	
+				</div>
+				
+			</div>	
+		
+		<div class="mainPage__icon mb-5">
+			<MainPageArrowDown></MainPageArrowDown>
+		</div>
 		<!-- </div> -->
 	</section>
 
-	<section class="gallery relative h-screen" parallaxCarusel inverse ref='galleryParallax'>
-		<div class="gallery__bg gallery__container sticky top-0 h-screen overflow-hidden">
-			<div class="gallery__content md:flex p-4 px-0 absolute t-0 w-full min-h-full lg:p-0 ">
+	<section class="gallery relative bg-[#121212]" inverse parallaxCarusel>
+		<div class="gallery__slider gallery__container sticky top-0 h-screen overflow-hidden" >
+			<div class="gallery__content absolute t-0 w-full md:flex p-4 px-0 min-h-full lg:p-0 ">
 				<div class="gallery__contentText flex lg:g-2 flex-col justify-center items-center ">
 					<h3 class="md:text-4xl font-semibold mb-2 mt-2 md:mb-4 text-xl">
 						How to Play (Gameplay) Fun
@@ -37,12 +47,12 @@
 					<img class="rounded-2xl" src="@/assets/img/1.jpg" alt="1">
 				</div>
 			</div>
-			<div class="gallery__content md:flex p-4 px-0 absolute t-0 w-full min-h-full lg:p-o opacity-0">
+			<div class="gallery__content absolute t-0 w-full md:flex p-4 px-0 min-h-full lg:p-o ">
 				<div class="gallery__contentText flex flex-col justify-center items-center ">
-					<h3 class="lg:text-4xl mb-4">
+					<h3 class="md:text-4xl font-semibold mb-2 mt-2 md:mb-4 text-xl">
 						DPS LETSGOO
 					</h3>
-					<p class="lg:text-xl">
+					<p class="lg:text-xl px-6 mb-4">
 						The advantages of Keqing Genshin Impact as Play DPS also because it doesn't need energy cost large enough to use Elemental Burst.
 			Keqing only requires an Energy Cost of 40 to use burst. Many other DPS require Energy Cost 80 so it will be difficult to do spamming burst or requires a Battery character.
 			The Cooldown and Internal Cooldown of Keqing's Elemental Skills are also short in duration so you can use them at any time. 
@@ -60,8 +70,9 @@
 	</section>
 	
 	<section class="bigGallery pt-2 pb-2 md:p-10">
-		<h2 class="text-center text-4xl font-semibold md:text-6xl"> More characters </h2>
+		<h2 class="text-center text-4xl font-semibold md:text-6xl"> Some Block </h2>
 		<div class="bigGallery__grid gridBigGallery">
+			
 		</div>
 	</section>
 </template>
@@ -72,7 +83,7 @@ import HeaderNavigation from '@/components/HeaderNavigation.vue';
 //---------my-imports--------------------//
 import $ from 'jquery';
 import {screenParallax} from '@/assets/js/screenParallax.js';
-import {onMounted, onUnmounted, onUpdated, ref} from 'vue';
+import {onMounted, onUnmounted, onUpdated} from 'vue';
 
 export default {
 	name: 'MainPage',
@@ -89,67 +100,60 @@ export default {
 			
 		});
 		onUpdated(() => {
-
 			handleSliderScroll();
 		})
 		onUnmounted(() => {
 			handleSliderScroll();
 			$(window).unbind('scroll', function() {
 				screenParallax($(".mainPage"));
+				
 			});
 		});
 		//-----Slider parallax?
-		const galleryParallax = ref(null);
 		function handleSliderScroll() {
-			let $parallaxCarusel = $("[parallaxCarusel]");
-			let $slides = $(".gallery__content");
-			let $slidesLength = $slides.length;
-			//
-			// let activeSlide = 0;
+			const $parallaxCarusel = $("[parallaxCarusel]");
+			const $slides = $(".gallery__content");
+			const $slidesLength = $slides.length;
+			const whenToShow = 3;
 			
-			// let heightBlock = $parallaxCarusel.height();
-			const posotionAnimation = 3; 
-			let $slideHeight = $($slides[0]).height();
+			let activeSlide = 0;
 
-			// console.log($(window).scrollTop() + $(window).height())
-			// console.log(($slideHeight + $slideHeight / posotionAnimation));
-			$parallaxCarusel.height(`${100 * $slidesLength}vh`);
+			$parallaxCarusel.height(`${100 * $slidesLength}vh`); // set height of block
+
+			// Hidden slides
+			$($slides).css({
+				"opacity": "1",
+			});
 			
-			if(($(window).height() + $(window).scrollTop()) >= ($slideHeight + ($slideHeight / posotionAnimation))){
-				//showSlide(activeSlide);
-			} else {
-				//showSlide(-1);
+			let itemPositionShow = $(window).height() - $($parallaxCarusel).offset().top / whenToShow;
+			if($(window).scrollTop() > $parallaxCarusel.offset().top - itemPositionShow) {
+				showSlide(activeSlide);
 			}
-
-			// function showSlide(activeSlide) {
-			// 	$($slides).css({
-			// 		"opacity": "0",
-			// 	});
-			// 	$($slides[activeSlide]).css({
-			// 		"opacity":'1',
-			// 	})
+			// function convert(value,From1,From2,To1,To2) {
+			// 	let tmp = (value - From1) / (From2 - From1)*(To2 - To1) + To1
+			// 	if(tmp > 1) {
+			// 		tmp = 1;
+			// 	}
+			// 	if(tmp < 0.1) {
+			// 		tmp = 0;
+			// 	}
+			// 	return tmp;
 			// }
-
-
-			if($parallaxCarusel.offset().top <= $(window).scrollTop()) {
-
-				if($(window).scrollTop() - $slideHeight >= $slideHeight / $slidesLength) {
-					// activeSlide++;
-					//showSlide(activeSlide);
-				}
-
-			} else if($(window).scrollTop() < $parallaxCarusel.offset().top) {
-				// console.log('top');
-			} else if ($(window).scrollTop() > ($parallaxCarusel.offset().top + $parallaxCarusel.height)) {
-				// console.log('down');
+			
+			function showSlide(activeSlide) {
+				//let tmp = convert($(window).scrollTop(),($parallaxCarusel.offset().top - itemPositionShow),($parallaxCarusel.offset().top),0,1)
+				//console.log(tmp);
+				$($slides[activeSlide]).css({
+					"opacity": `1`,
+				})
+				
 			}
-			// console.log($(window).scrollTop(),$parallaxCarusel.offset().top + $parallaxCarusel.height())
-			
-			
+
+		
 		}
 
+
 		return {
-			galleryParallax,
 			handleSliderScroll,
 		}
 
@@ -166,16 +170,32 @@ window.addEventListener('resize', () => {
 </script>
 
 <style lang="scss">
+	.slide-text-enter-from {
+		transform: translateY(100px);
+		opacity: 0;
+	}
+	.slide-text-enter-active {
+		transition: all 1s ease 0.1s;
+	}
+	
+	.slide-text-enter-active .mainPage__subTitle{
+	
+	}
+	.slider-text-enter-to {
+		opacity: 1;
+		transform: translateY(0px);
+	}
+
 	.mainPage {
 		background-color: #fdfdfd;
 		height: calc(var(--vh,1vh)*100);
-		transition: transform .1s;
+		transition: transform .0s linear;
 		
 	}
 	.gallery {
 		color: #fdfdfd;
-		&__bg {
-			background-color: #121212;
+		&__slider {
+			//background-color: #121212;
 		}
 		&__contentText {
 			flex: 1 60%;
@@ -188,5 +208,7 @@ window.addEventListener('resize', () => {
 				object-position: center;
 			}
 		}
+		
 	}
+
 </style>
